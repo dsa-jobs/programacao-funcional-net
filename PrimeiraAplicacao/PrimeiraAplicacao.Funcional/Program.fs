3f0d990077160b881d125bd843bd7b5d4c6bb581
Daniel Sena <dsa.jobs@yahoo.com>
2021-08-19 21:25:12 -0300

    adiciona os exemplos do capítulo um

---
+++ PrimeiraAplicacao/PrimeiraAplicacao.Funcional/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+
+let olaMundo() =
+    printfn "Olá Mundo"
+
+[<EntryPoint>]
+let main argv =
+    olaMundo()
+    0
---
+++ PrimeiraAplicacao/PrimeiraAplicacao/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeiraAplicacao
+{
+    class Calculadora
+    {
+        public int ElevarNumeroAoQuadrado(int numero)
+        {
+            return numero * numero;
+        }
+
+        public int SomaQuadradosDosNumerosAteDez()
+        {
+            return Enumerable.Range(1, 10)
+                .Select(ElevarNumeroAoQuadrado)
+                .Sum();
+        }
+    }
+}
